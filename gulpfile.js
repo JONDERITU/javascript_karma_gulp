47447cdb897241e093db4de1a5cf6e08be52c2f2
INTERN TWO <Interntwo@JGichane.local>
2016-03-24 10:13:59 +0300

    added jshint plugin and added task on gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,16 @@
 */
 //include gulp
 var gulp = require('gulp');
-	
+
+//include jshint plugin
+var jshint = require('jshint');
+
+//Jshint Task
+gulp.task('jshint', function(){
+	gulp.src('/src/scripts/*.js')
+		.pipe(jshint())
+		.pipe(jshint.reporter('default'));
+});
 gulp.task ('default', function(){
 console.log('hello world!');
 });
