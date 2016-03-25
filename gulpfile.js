bef86f09e7f3b8744684ef3dd06e3000f330daf4
INTERN TWO <Interntwo@JGichane.local>
2016-03-25 15:09:22 +0300

    finalised

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,23 @@
 var gulp = require('gulp');
 
 //include jshint plugin
-var jshint = require('jshint');
+ var jshint = require('jshint');
+
+//include ruby sass
+var sass = require("gulp-ruby-sass")
 
 //Jshint Task
 gulp.task('jshint', function(){
-	gulp.src('/src/scripts/*.js')
+	gulp.src('src/scripts/*.js')
 		.pipe(jshint())
 		.pipe(jshint.reporter('default'));
 });
 gulp.task ('default', function(){
 console.log('hello world!');
+});
+//sass task
+gulp.task('styles', function() {
+  return sass('src/styles/main.scss', { style: 'expanded' })
+    .pipe(gulp.dest('src/styles/main.css'))
+    
 });
